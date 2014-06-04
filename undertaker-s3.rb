4c5b0e582b032d22bc724b5adcfab4a1a85a387f
Carlos Peñas <carlos.penas@the-cocktail.com>
2014-06-04 17:39:04 +0200

    opts as a global

--- undertaker-s3.rb
+++ undertaker-s3.rb
@@ -14,9 +14,19 @@ require "colorize"
 
 REPOSITORY_FILE = File.dirname(__FILE__) + '/github_repos.yml'
 
+$opts = Trollop::options do
+  opt :n, 'dry-run', :default => false   # flag dry-run
+  opt :repo, 'Clone Repository', :type => :string
+  opt :bucket, 'Bucket name', :type => :string
+  opt :debug, 'debug flag', :default => false
+  opt :aws_access_key_id, 'AWS access key ID', :default => ENV['AWS_ACCESS_KEY_ID']
+  opt :aws_secret_access_key, 'AWS secret access key', :default => ENV['AWS_SECRET_ACCESS_KEY']
+end
+
+
 AWS::S3::Base.establish_connection!(
-    :access_key_id     => opts[:access_key_id],
-    :secret_access_key => opts[:secret_access_key],
+    :access_key_id     => $opts[:access_key_id],
+    :secret_access_key => $opts[:secret_access_key],
     :use_ssl => USE_SSL
   )
 
@@ -28,11 +38,11 @@ end
 
 def  clone_and_upload_to_s3(options)
 	 puts "\n\nChecking out #{options[:name]} ...".green
-	 clone_command = "cd #{opts[:bucket]} && git clone --bare #{options[:clone_url]} #{options[:name]}"
+	 clone_command = "cd #{$opts[:bucket]} && git clone --bare #{options[:clone_url]} #{options[:name]}"
    puts clone_command.yellow
    system(clone_command)
 	 puts "\n Compressing #{options[:name]} ".green
-	 system("cd #{opts[:bucket]} && tar czf #{compressed_filename(options[:name])} #{options[:name]}")
+	 system("cd #{$opts[:bucket]} && tar czf #{compressed_filename(options[:name])} #{options[:name]}")
 	 
 	 upload_to_s3(compressed_filename(options[:name]))
 	 
@@ -45,7 +55,7 @@ def  clone_and_upload_to_s3(options)
  def upload_to_s3(filename)
 	 begin
 		puts "** Uploading #{filename} to S3".green
-		path = File.join(opts[:bucket], filename)
+		path = File.join($opts[:bucket], filename)
 		S3Object.store(filename, File.read(path), s3bucket)
 	 rescue Exception => e
 		puts "Could not upload #{filename} to S3".red
@@ -85,7 +95,7 @@ def ensure_bucket_exists
  end
 
 def s3bucket
-	s3bucket = opts[:bucket]
+	s3bucket = $opts[:bucket]
 end
 
 
@@ -104,25 +114,21 @@ def backup_repos
   back_repos_from_arguments
 end
 
-opts = Trollop::options do
-  opt :n, 'dry-run', :default => false   # flag dry-run
-  opt :repo, 'Clone Repository', :type => :string
-  opt :bucket, 'Bucket name', :type => :string
-  opt :debug, 'debug flag', :default => false
-  opt :aws_access_key_id, 'AWS access key ID', :default => ENV['AWS_ACCESS_KEY_ID']
-  opt :aws_secret_access_key, 'AWS secret access key', :default => ENV['AWS_SECRET_ACCESS_KEY']
-end
+puts "options: " if $opts[:debug]
+p $opts if $opts[:debug]
 
-puts "options: " if opts[:debug]
-p opts if opts[:debug]
+Trollop::die :repo, ' Need a repo to clone' unless $opts[:repo]
+Trollop::die :aws_secret_access_key, ' Secret access key needed' unless $opts[:aws_secret_access_key]
+Trollop::die :aws_access_key_id, ' Access key needed' unless opts[:aws_access_key_id]
+Trollop::die :bucket, ' need a bucket to upload' unless opts[:bucket]
 
 begin
 	# create temp dir
-	Dir.mkdir(opts[:bucket]) rescue nil
+	Dir.mkdir($opts[:bucket]) rescue nil
 	ensure_bucket_exists
 	backup_repos
 ensure	
 	# remove temp dir
-	delete_dir_and_sub_dir(opts[:bucket])
+	delete_dir_and_sub_dir($opts[:bucket])
 end
 
